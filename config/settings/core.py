cd7f4ca6759ef62af0e9ae52dcf9f34dbede564c
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-07-29 20:47:44 +0330

    feat(settings): postgres configs

--- config/settings/core.py
+++ config/settings/core.py
@@ -54,15 +54,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,16 @@
 from .core import *  # noqa
+from decouple import config
 
 DEBUG = True
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
