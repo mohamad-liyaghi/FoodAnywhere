18eea08c098d9b5633d01dd34dbd4a7e40ff5edb
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-07-31 13:29:40 +0330

    feat(celery): configs and container

---
+++ config/celery.py
@@ -0,0 +1,9 @@
+from celery import Celery
+from decouple import config
+
+celery = Celery("config", broker=config("CELERY_BROKER_URL"))
+celery.config_from_object("django.conf:settings", namespace="CELERY")
+celery.config_defaults = {"broker_connection_retry_on_startup": True}
+
+
+celery.autodiscover_tasks()
