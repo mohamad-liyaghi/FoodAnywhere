f3cf0813247d6b29d515e5aac50ffe8b58475afa
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-02 15:19:04 +0330

    feat(active_sessions): fixtures

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest  # noqa
 from rest_framework.test import APIClient
 from apps.users.tests.fixtures import *  # noqa
+from apps.active_sessions.tests.fixtures import *  # noqa
 
 
 @pytest.fixture(scope="class")
