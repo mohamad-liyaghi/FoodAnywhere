1d54b41017c067117eafc4cd9463fd21d93438fe
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-02 15:09:47 +0330

    feat: gunicorn

--- docker/scripts/backend.sh
+++ docker/scripts/backend.sh
@@ -5,4 +5,8 @@ python manage.py makemigrations
 python manage.py migrate
 
 echo "Starting the server..."
-python manage.py runserver 0.0.0.0:8000
+if [[ $ENVIRONMENT == "LOCAL" ]]; then
+  python manage.py runserver 0.0.0.0:8000
+else
+  gunicorn config.wsgi --bind 0.0.0.0:8000 --timeout 200 --threads=3 --worker-connections=1000
+fi
