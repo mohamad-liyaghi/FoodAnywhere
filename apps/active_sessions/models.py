e3218278f6257eb91db525bf58c1fab593a07fff
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-02 15:18:18 +0330

    feat(active_sessions): Active Session model

--- apps/active_sessions/models.py
+++ apps/active_sessions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from active_sessions.enums import LoginDeviceType, LoginBrowserType
 
-# Create your models here.
+
+class ActiveSession(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="active_sessions",
+    )
+    device_type = models.CharField(max_length=1, choices=LoginDeviceType.choices, default=LoginDeviceType.UNKNOWN)
+    browser_type = models.CharField(max_length=1, choices=LoginBrowserType.choices, default=LoginBrowserType.UNKNOWN)
+    ip_address = models.GenericIPAddressField()
+    is_deleted = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.ip_address} - {self.date}"
+
+    class Meta:
+        ordering = ["-date"]
+        verbose_name = "Active Session"
+        verbose_name_plural = "Active Sessions"
