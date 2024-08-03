c93e1eb46089b33a2c50129ff2254f3bed4f9565
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-03 14:24:38 +0330

    feat(locations): Location Model

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.conf import settings
+from uuid import uuid4
 
-# Create your models here.
+
+class Location(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="locations")
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    location = models.PointField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["created_at"]
