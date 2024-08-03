1db4fa4946dfe1395218043b47306c555edc8d22
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-03 19:25:18 +0330

    feat(locations): create view

--- apps/locations/views.py
+++ apps/locations/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema_view, OpenApiResponse, extend_schema
 from locations.models import Location
@@ -15,10 +15,24 @@ from locations.serializers import LocationSerializer
         },
         tags=["Locations"],
     ),
+    post=extend_schema(
+        summary="Create Location",
+        description="Create a location for the logged in user.",
+        request=LocationSerializer,
+        responses={
+            201: LocationSerializer,
+            400: OpenApiResponse(description="Bad Request"),
+            403: OpenApiResponse(description="Unauthorized"),
+        },
+        tags=["Locations"],
+    ),
 )
-class LocationListCreateView(ListAPIView):
+class LocationListCreateView(ListCreateAPIView):
     serializer_class = LocationSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Location.objects.select_related("user").filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
