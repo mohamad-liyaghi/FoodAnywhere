a770529e99587b737e23377ee31f433a6b544748
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-04 12:15:02 +0330

    feat(restaurants): Restaurant model

---
+++ apps/restaurants/enums.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class RestaurantStatus(models.TextChoices):
+    REQUESTED = "r", "Requested"
+    APPROVED = "a", "Approved"
+    DENIED = "d", "Denied"
+    CANCELLED = "c", "Cancelled"
--- apps/restaurants/models.py
+++ apps/restaurants/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.conf import settings
+from uuid import uuid4
+from restaurants.enums import RestaurantStatus
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="restaurants")
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    location = models.PointField()
+    longitude = models.FloatField(blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    status = models.CharField(
+        max_length=1,
+        choices=RestaurantStatus.choices,
+        default=RestaurantStatus.REQUESTED,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
