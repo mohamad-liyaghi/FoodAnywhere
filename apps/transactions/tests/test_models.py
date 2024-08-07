53da3771cab17748f4c0b8528cae031e74a0e9eb
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-07 13:05:13 +0330

    feat(transactions): model tests

---
+++ apps/transactions/tests/test_models.py
@@ -0,0 +1,26 @@
+import pytest
+from transactions.models import Transaction
+from transactions.enums import TransactionType, TransactionStatus
+
+
+@pytest.mark.django_db
+class TestTransactionModel:
+    def test_user_balance_increased_after_successful_deposit(self, user):
+        user_balance = user.balance
+        successful_deposit = Transaction.objects.create(
+            user=user,
+            amount=100.00,
+            type=TransactionType.DEPOSIT,
+            status=TransactionStatus.SUCCESS,
+        )
+        assert user.balance == user_balance + successful_deposit.amount
+
+    def test_user_balance_decreased_after_successful_withdrawal(self, user):
+        user_balance = user.balance
+        successful_withdrawal = Transaction.objects.create(
+            user=user,
+            amount=100.00,
+            type=TransactionType.WITHDRAWAL,
+            status=TransactionStatus.SUCCESS,
+        )
+        assert user.balance == user_balance - successful_withdrawal.amount
