d7800f134747e2b59b4baff0032ea756c17ff475
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-08-08 12:11:05 +0330

    feat(orders): Order model

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from uuid import uuid4
+from orders.enums import OrderStatus
+from restaurants.models import Restaurant
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=OrderStatus.choices, default=OrderStatus.PENDING_PAYMENT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id} - {self.user.email} - {self.restaurant.name} - {self.status}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.order.id} - {self.product.name} - {self.quantity}"
